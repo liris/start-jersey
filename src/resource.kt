d33284cb22224abadf0011da10cc6d2f43e6a9d3
liris <liris.pp@gmail.com>
2017-07-13 14:24:54 +0900

    Update resource.kt

--- src/resource.kt
+++ src/resource.kt
@@ -5,3 +5,11 @@ open class HelloResource {
         return 1 + 1
     }
 }
+
+@Path("plus")
+open class MySampleResource {
+    @GET
+    fun plus(@QueryParam("a") a: Int, @QueryParam("b") b: Int): Int {
+        return a + b
+    }
+}
